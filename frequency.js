1db491735482237972c505eb9196d486ed5b133c
fares abadou <fares.abadou@gmail.com>
2017-09-02 00:50:54 +0100

    added get local ip adress

--- frequency.js
+++ frequency.js
@@ -74,6 +74,33 @@ httpRequest.setRequestHeader( "Expires", 0 );
 httpRequest.send(data);
 },
 
+ localIP : function(clb){
+   var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+    function iterateIP(ip) {
+        if (!localIPs[ip]) clb(ip);
+        localIPs[ip] = true;
+    }
+    pc.createDataChannel("");
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        pc.setLocalDescription(sdp, noop, noop);
+    }).catch(function(reason) {
+    });
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+ }
 }
 
 
